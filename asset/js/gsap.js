17800b28d8745e1eedc1bb60f955d2fcd4a0176c
Maxime Perraud <contact@maxime-p.dev>
2023-05-23 14:39:19 +0200

    feat: automatic rotation

--- asset/js/gsap.js
+++ asset/js/gsap.js
@@ -5,6 +5,7 @@ import MotionPathPlugin from "gsap/MotionPathPlugin";
 gsap.registerPlugin(MotionPathPlugin);
 
 
+
 // Set default position for planets
 gsap.to("#planet0", {
     duration: 0,
@@ -46,74 +47,29 @@ gsap.to("#planet3", {
     }
 })
 
-function play(selectedPlanet) {
-    let target = -selectedPlanet
-
-    if (target === -3){
-        target = 1
-    }
-
-    console.log(selectedPlanet, target)
-    if (target === 0){
-        return
-    }
-    gsap.to('[data-value="0"]', {
-        duration: 2,
-        motionPath: {
-            path: "#path",
-            align: "#path",
-            start: 0.25,
-            end: (target+1) * 0.25,
-            alignOrigin: [0.5, 0.5],
-        }
-    })
-    gsap.to('[data-value="1"]', {
-        duration: 2,
-        motionPath: {
-            path: "#path",
-            align: "#path",
-            start: 0.50,
-            end: (target+2) * 0.25,
-            alignOrigin: [0.5, 0.5],
-        }
-    })
-    gsap.to('[data-value="2"]', {
-        duration: 2,
-        motionPath: {
-            path: "#path",
-            align: "#path",
-            start: 0.75,
-            end: (target+3) * 0.25,
-            alignOrigin: [0.5, 0.5],
-        }
-    })
-    gsap.to('[data-value="3"]', {
-        duration: 2,
-        motionPath: {
-            path: "#path",
-            align: "#path",
-            start: 1,
-            end: (target+4) * 0.25,
-            alignOrigin: [0.5, 0.5],
-        }
-    })
-}
 
 document.addEventListener("DOMContentLoaded", () => {
     const planets = document.querySelectorAll(".planet")
 
-    function updateValues(selectedPlanet){
-        planets.forEach(planet => {
-            planet.dataset.value = ((planet.dataset.value-selectedPlanet+4)%4).toString()
-        })
-    }
+    const tl = gsap.timeline()
 
     planets.forEach(planet => {
+        const id = planet.dataset.value
+        console.log(document.querySelector(`[data-value="${id}"]`))
+        tl.to(`[data-value="${id}"]`, {
+            duration: 15,
+            ease: "none",
+            repeat: -1,
+            motionPath: {
+                path: "#path",
+                align: "#path",
+                start: id * 0.25,
+                end: (id * 0.25) + 1,
+                alignOrigin: [0.5, 0.5]
+            }
+        }, 0)
         planet.addEventListener("click", () => {
-            // Update locations of planets
-            play(planet.dataset.value)
-            // Update planet selector for next interaction
-            updateValues(planet.dataset.value)
+            tl.pause()
         })
     })
 })

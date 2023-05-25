48aeb2c1929ea742d34a55cde36529dfb23b3e26
Maxime Perraud <contact@maxime-p.dev>
2023-05-25 09:37:45 +0200

    fix: trying to fix scroll issue

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -1,19 +1,20 @@
 import {gsap} from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger.js"; 
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 
 gsap.registerPlugin(ScrollTrigger);
 
-let reveal = document.querySelectorAll(".page") 
+let reveal = document.querySelectorAll(".content")
 
 reveal.forEach( (el) => {
     let headings = el.querySelectorAll("h2, p")
-    
+
     let tl = gsap.timeline ()
     .from(headings, {y:80, stagger:0.05, opacity:0, duration:1, ease:"power3.out"})
-    
+
     ScrollTrigger.create ({
+        scroller: ".page",
         trigger: el,
-        start: "center 100%", 
+        start: "center 100%",
         end:"top 50%",
         markers: true,
         toggleActions: "play none none reverse ",

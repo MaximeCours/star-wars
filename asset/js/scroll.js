2491478f145d4303d5549d0d47ded5649e864525
TomGOURDY <tom.gourdy@edu.devinci.fr>
2023-05-25 14:12:23 +0200

    fixed scroll assets

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -9,7 +9,7 @@ const pages = document.querySelectorAll(".page")
 pages.forEach((page) => {
     let reveal = page.querySelectorAll(".content")
     reveal.forEach( (el) => {
-        let headings = el.querySelectorAll("h2, p")
+        let headings = el.querySelectorAll("h2, p, div")
     
         let tl = gsap.timeline ()
         .from(headings, {y:80, stagger:0.05, opacity:0, duration:1, ease:"power3.out"})
